d810bd07d5df4929425068b262122d7a7323221b
stephanie <stephanie.somade@news.co.uk>
2025-07-18 22:46:20 +0100

    chore(setup): initialize Vite React app and configure React Router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
+import Contact from './components/Contact'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  )
+}
