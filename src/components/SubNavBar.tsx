c0511aa13fc28bef2e0a07d204899d9d2357b07f
stephanie <stephanie.somade@news.co.uk>
2025-07-19 14:24:51 +0100

    feat: Add new subnav bar

---
+++ src/components/SubNavBar.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+import '../styles/SubNavBar.css'
+
+function SubNavBar() {
+
+  return (
+    <nav className="subnavbar">
+      <div className="subnav-links">
+        <NavLink to="/resume" end className={({ isActive }) => isActive ? "active" : ""}>Bio</NavLink>
+        <NavLink to="/testimonials" className={({ isActive }) => isActive ? "active" : ""}>Portfolio</NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Contact Me</NavLink>
+      </div>
+    </nav>
+  );
+}
+export default SubNavBar;
